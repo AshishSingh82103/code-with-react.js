385ca417e2f1c4cc6940a45b5a8b426362a3661c
Ashish Singh <singhashish82103@gmail.com>
2024-06-05 09:30:42 +0530

    java script refresher

---
+++ javascript-refresher/index.js
@@ -0,0 +1,100 @@
+// let a = 34;
+// console.log(a);
+
+// let b = "this is me";
+// console.log(b);
+
+// -----var, let, const----------
+
+// number, object, boolean, string, null, undefined, BigInt
+
+// function----------
+
+// function hello() {
+//     console.log('singhashish');
+// }
+// hello();
+
+// Object-----------
+
+// let obj = {
+//     r: 34,
+//     b: 45,
+//     g: 42
+// }
+// console.log(obj.g); // 42
+
+// console.log(typeof obj); // object
+
+// console.log(typeof obj. r); // number
+
+// let obja = {
+//    r: 443,
+//    b: 43,
+//    c: 54,
+//    func: function myfunct(number) {
+//     console.log(`The number is ${number}`)
+    
+//    }
+
+        
+// }
+// obja.func(45); // The number is 45
+
+
+// let function1 = function(num1, num2) {
+//    return num1 + num2
+
+// }
+// console.log(function1(43, 41));
+
+// let obja = {
+//    userId: "singhashish@82103",
+//    password: "singh@8989",
+//    isUserLoggedIn: true,
+//    func: function myfunction(number1, number2) {
+//       console.log(`This is the main calculation ${number1 + number2}`)
+      
+//    }
+// }
+// console.log(obja.userId); // singhashish@82103
+// obja.func(43,55); // 98
+
+// let a = 34;
+// let b = 56;
+// let result = a + b;
+// console.log(result); // 90
+
+// let userId =  "singhashish@gmail.com";
+// let isUSerLoggedIn = true;
+
+// if(isUSerLoggedIn == false) {
+//    console.log(isUSerLoggedIn);
+// } else {
+//    console.log(userId);
+// }
+
+
+// document.addEventListener("click", function click() {
+//    console.log("clicked");
+//    // alert("hello");
+//    let conf = confirm("Are you sure");
+//    console.log(conf);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
